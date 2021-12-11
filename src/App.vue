5ad89a4237472caf21ae9624673e9730bfafc6dc
ChrisD921 <cdrape921@gmail.com>
2021-12-11 17:17:20 +0800

    Experimenting #1
    Testing out ways to go about development.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-    <div id="app">
-
-        <router-view></router-view>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="jumbotron">
+          <div class="container">
+            <span class="display-4 h1">Vue.js Blog Tutorial #1</span>
+            <p class="lead">Simple example how to connect to the server, paginate results, create single page app...</p>
+          </div>
+        </div>
+     </div>
+     <footer class="col-sm-12">
+       <hr>
+       
+     </footer>
     </div>
+  </div>
 </template>
 
-<script>
-    export default {
-        name: 'app',
-    }
-</script>
 
