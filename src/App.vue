882598edf23f6058f6f11ecd626eff5d09d35fb7
ChrisD921 <cdrape921@gmail.com>
2021-12-15 15:53:28 +0800

    Creating News Page and Display Correct Data
    The page is working. The layout could use some polish.
    
    The headline, content, and image are stored in Json-Server.
    
    I placed 3 different articles in the Json- Server and all of them can be displayed on the page if the index number is changed in App.vue.

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,143 @@
       <div class="col-sm-12">
         <div class="jumbotron">
           <div class="container">
-            <span class="display-4 h1">Vue.js Blog Tutorial #1</span>
-            <p class="lead">Simple example how to connect to the server, paginate results, create single page app...</p>
+            <span class="display-4 h1">USC Specific News Page</span>
+            <p class="lead">To USC We Sing</p>
           </div>
         </div>
      </div>
+     <body>
+       <div class="wrapper">
+          <div class="leftcolumn">
+            <div class="headline">
+            <h3> {{articles[0].headline}} </h3>
+            </div>
+            <div class="content">
+              <p>{{articles[0].content}}</p>
+            </div>
+          </div>
+          
+          <div class="rightcolumn">
+            <img :src='articles[0].image' alt="Article Image">
+          </div>
+          
+       </div>
+     </body>
      <footer class="col-sm-12">
        <hr>
        
      </footer>
     </div>
+    
+    
+    <!-- <Articles :articles="articles" /> -->
+    
+    
+    <!-- <div :key="article.id" v-for="article in articles"> -->
+            <!-- <h3> {{article.headline}}</h3> -->
+            <!-- <img :src="`/assets/img/`+article.image" alt="park"> -->
+            <!-- <img :src='article.image' alt="Article Image"> -->
+            <!-- <p>{{article.content}}</p> -->
+    <!-- </div> -->
+    
+ 
+    
+    
+
   </div>
 </template>
 
+<script>
+import Articles from './components/dcism/NewsPageSpecific/Articles'
 
+export default {
+  name: 'App',
+  components: {
+    // Articles,
+  },
+  data() {
+    return {
+      articles: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/articles')
+    .then(res => res.json())
+    .then(data => this.articles = data)
+    .catch(err => console.log(err.message))
+  },
+  methods() {
+  },
+  created() {
+    // this.articles = [
+    //   {
+    //     id:1,
+    //     headline: 'Doctors Appointment',
+    //     content: 'blah blah blah 1',
+    //     image: 'ArrowGuy.jpg',
+    //   },
+    //   {
+    //     id:2,
+    //     headline: 'Meeting at School',
+    //     content: 'March 3rd at 1:30pm',
+    //     image: 'Park.jpg',
+    //   },
+    //   {
+    //     id:3,
+    //     headline: 'Food Shopping',
+    //     content: 'March 3rd at 11:00am',
+    //     image: 'Gorilla.png',
+    //   },
+    // ]
+  }
+}
+</script>
+
+<style scoped>
+
+/* .image {
+  padding-top: 5%;
+  padding-bottom: 5%;
+}
+
+body .image img{
+  display: block;
+  margin-left: auto;
+  width: 50%;
+} */
+
+img{
+  display: block;
+  width: 670px;
+  
+} 
+
+body {
+    background-color: #228B22;
+    margin: 0 auto;
+    
+}    
+.wrapper {
+      width: 1120px;
+      margin: 0 auto;
+      
+      
+}
+.leftcolumn, .rightcolumn {
+    border: 1px solid #228B22;
+    float: left;
+    min-height: 450px;
+    
+}
+.leftcolumn {
+      width: 450px;
+      background-color: #DCDCDC;
+}
+.rightcolumn {
+      width: 500px;
+      margin: 0 auto;
+}
+
+
+
+</style>
---
+++ src/components/dcism/NewsPageSpecific/Articles.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <!-- <img :src="'./public/Park.jpg'"> -->
+        <div :key="article.id" v-for="article in articles">
+            <h3> {{article.headline}}</h3>
+           
+            
+             <!-- <img src="@/assets/img/Park.jpg">  --> <!-- WORKS -->
+             <!-- <img src="/public/img/Park.jpg"> -->
+             <!-- <img src="/public/img/Park.jpg"> -->
+             <!-- <img :src="`{{article.image}}`"> -->
+             <!-- <img :src="article.image" > -->
+             <!-- <img src="article.image"> -->
+            <!-- {{article.image}} -->
+            <!-- <img src="/assets/img/Park.jpg" alt="Article Image">  -->
+            <!-- <img src="`@/assets/img/${article.image}`"/> -->
+             <!-- <img v-bind:src="article.image" alt="Article Image">  -->
+            <!-- {{article.image}} -->
+           <img v-bind:src="'/assets/img/' + article.image" /> 
+           <img :src="`/assets/img/`+article.image">
+
+           <img :src="`../assets/img/`+article.image">
+           
+            <p> {{article.content}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Articles',
+    props: {
+        articles: Array,
+    },
+}
+</script>
+
+
