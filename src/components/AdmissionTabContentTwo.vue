09a75c213751c61d66bf894e8763c057f0c14f01
JaedQ <mafuyuquimchee@gmail.com>
2022-04-01 21:28:40 +0800

    IT-12 API

--- src/components/AdmissionTabContentTwo.vue
+++ src/components/AdmissionTabContentTwo.vue
@@ -28,7 +28,7 @@
         methods: {
             getData() {
                 let self = this;
-                axios.get('http://127.0.0.1:8088/api/users').then(res=>{
+                axios.get('http://127.0.0.1:8088/api/IT12').then(res=>{
                     if(res.status==200){
                     self.retData = res.data;
                     window.console.log(this.retData.admission[1].img);
